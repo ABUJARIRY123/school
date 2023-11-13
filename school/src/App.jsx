dd472c099d99223a5c4565e5844743523919bb82
Maro Ridhiwani <maroridhiwani@gmail.com>
2023-11-13 08:27:37 +0300

    slideshows

--- school/src/App.jsx
+++ school/src/App.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import {Navbar, Home, About, Teachers, Programs, Contact, Footer} from './components/index';
+import { Navbar, Home, About, Teachers, Programs, Contact, Footer } from './components/index';
+import SlideShow from './components/SlideShow';
+import slide1 from '../src/assets/1.jpg';
+import slide2 from '../src/assets/2.jpg';
+import slide3 from '../src/assets/3.jpg';
+import slide4 from '../src/assets/4.jpg';
+
+const slides = [
+  { id: 1, src: slide1 },
+  { id: 2, src: slide2 },
+  { id: 3, src: slide3 },
+  { id: 4, src: slide4 },
+];
 
 function App() {
-
   return (
-<div className="font-Poppins bg-Solitude">
-  <Navbar/>
-  <Home/>
-  <About/>
-  <Programs/>
-  <Teachers/>
-  <Contact/>
-  <Footer/>
-  
-</div>
+    <div className="font-Poppins bg-Solitude">
+      <Navbar />
+      <div className="max-w-screen-xl mx-auto">
+        <SlideShow>
+          {slides}
+        </SlideShow>
+      </div>
+      <Home />
+      <About />
+      <Programs />
+      <Teachers />
+      <Contact />
+      <Footer />
+    </div>
   );
-      
 }
 
-export default App
+export default App;
--- school/src/components/Navbar/Navbar.jsx
+++ school/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import NavLink from './NavLink'
-import { navLinks } from '../../Data'
-import {HiMenu, HiX} from 'react-icons/hi'
-import MobileNavLink from './MobileNavLink'
+import React, { useEffect, useState } from 'react';
+import NavLink from './NavLink';
+import MobileNavLink from './MobileNavLink';
+import { navLinks } from '../../Data';
+import { HiMenu, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import {
   BsFacebook,
@@ -10,76 +10,75 @@ import {
   BsWhatsapp,
   BsEnvelope
 } from "react-icons/bs";
-
-import {FaXTwitter} from "react-icons/fa6"
+import { FaXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [active, setActive] = useState(null);
+
   useEffect(() => {
     const scrollActive = () => {
       setActive(window.scroll > 20);
     };
     window.addEventListener("scroll", scrollActive);
     return () => window.removeEventListener("scroll", scrollActive);
-  },[active]);
+  }, [active]);
+
+  const isDesktop = window.innerWidth > 1024;
+
   return (
-    <div className={`${active ? "shadow-lg bg-Solitude": ""}fixed w-full top-0 left-0 z-20` }>
-    <div className='bg-Teal p-0.03'>
-      <div  className={`${active ? "py-2 transition-all duration-300":"py-4"}
-      container mx-auto flex items-center justify-between px-2`}>
-<div className='flex items-center gap-5'>
-  <HiMenu className='text-3xl sm:hidden cursor-pointer ' onClick={() => setToggle(true)} />
-    <img src="/logo.png" alt="Logo" className="h-12 w-auto" /> {/* Add this line */}
-        <div className='text-xl text-black Uppercase tracking-wide font-bold'>
-Manarat International <br/>School of Kenya
-        </div>
-       </div>
-       <div className='sm:flex items-center hidden'>
-        {navLinks.map(navLink => {
-          return <NavLink key={navLink.id} {...navLink}/>
-        })}
-        
-       </div>
-       
-       <div className="flex gap-4 mt-4">
-           <a href="https://www.facebook.com/ManaratKenya/" className="hover:scale-110 text-xl">
+    <div className={`${active ? "shadow-lg bg-Solitude" : ""} fixed w-full top-0 left-0 z-20`}>
+      <div className='bg-Teal p-0.03 '>
+        <div className={`${active ? "py-2 transition-all duration-300" : "py-4"}
+          container mx-auto flex items-center justify-between px-2`}>
+          <div className='flex items-center gap-5'>
+            <HiMenu className='text-3xl sm:hidden cursor-pointer' onClick={() => setToggle(true)} />
+            {isDesktop && <img src="/logo.png" alt="Logo" className="h-12 w-auto cursor-pointer" />}
+          </div>
+
+          <div className='sm:flex items-center hidden'>
+            {navLinks.map(navLink => {
+              return <NavLink key={navLink.id} {...navLink} />
+            })}
+
+          </div>
+
+          <div className="flex gap-4 mt-4">
+            <a href="https://www.facebook.com/ManaratKenya/" className="hover:scale-110 text-xl">
               <BsFacebook />
             </a>
             <a href="https://www.instagram.com/misk.manarat/" className="hover:scale-110 text-xl">
               <BsInstagram />
             </a>
             <a href="https://twitter.com/manarat_misk" className="hover:scale-110 text-xl">
-<FaXTwitter />
-
+              <FaXTwitter />
             </a>
             <a href="https://wa.me/254795058718" className="hover:scale-110 text-xl">
               <BsWhatsapp />
             </a>
-             <a href="mailto:manarat.misk@gmail.com" className="hover:scale-110 text-xl">
+            <a href="mailto:manarat.misk@gmail.com" className="hover:scale-110 text-xl">
               <BsEnvelope />
             </a>
           </div>
-       {toggle && (
-        <motion.div 
-        initial={{x: -500, opacity:0}}
-        animate={{x:0, opacity: 1}}
-        transition={{duration:0.3}}
-        className='fixed h-full w-96 top-0 left-0 z-20 bg-Teal text-white 
-        flex flex-col justify-center items-center shadow-lg gap-8 py-8'>
-        {navLinks.map((navLink) => {
-          return <MobileNavLink key={navLink.id} {...navLink} setToggle={setToggle}/>;
-        })}
-        <HiX className='absolute right-12 top-12 text-3xl cursor pointer'
-          onClick={(prev) => setToggle(!prev)}
-        />
-       </motion.div>
-
-       )}
+          {toggle && (
+            <motion.div
+              initial={{ x: -500, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className='fixed h-full w-96 top-0 left-0 z-20 bg-Teal text-white 
+              flex flex-col justify-center items-center shadow-lg gap-8 py-8'>
+              {navLinks.map((navLink) => {
+                return <MobileNavLink key={navLink.id} {...navLink} setToggle={setToggle} />;
+              })}
+              <HiX className='absolute right-12 top-12 text-3xl cursor pointer'
+                onClick={(prev) => setToggle(!prev)}
+              />
+            </motion.div>
+          )}
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Navbar 
+export default Navbar;
